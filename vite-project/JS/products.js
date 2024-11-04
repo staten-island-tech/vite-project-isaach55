f423ab188ad1bbf8ea5d4490649186371bdd6cd8
isaach55 <isaach55@nycstudents.net>
2024-11-04 12:27:35 -0500

    array

---
+++ vite-project/JS/products.js
@@ -0,0 +1,24 @@
+//@export {key name};
+
+const products = [
+  { type: "patties", name: "beef", vegetarian: "no" },
+  { type: "patties", name: "brisket", vegetarian: "no" },
+  { type: "patties", name: "impossible", vegetarian: "yes" },
+  { type: "topping", name: "lettuce", vegetarian: "yes" },
+  { type: "topping", name: "mushrooms", vegetarian: "yes" },
+  { type: "topping", name: "onions", vegetarian: "yes" },
+  { type: "topping", name: "caramelized onions", vegetarian: "yes" },
+  { type: "topping", name: "pickles", vegetarian: "yes" },
+  { type: "topping", name: "tomato", vegetarian: "yes" },
+  { type: "topping", name: "slaw", vegetarian: "yes" },
+  { type: "topping", name: "fried egg", vegetarian: "yes" },
+  { type: "topping", name: "jalapenos", vegetarian: "yes" },
+  { type: "topping", name: "bacon", vegetarian: "no" },
+  { type: "cheese", name: "american cheese", vegetarian: "yes" },
+  { type: "cheese", name: "pepper jack", vegetarian: "yes" },
+  { type: "cheese", name: "swiss", vegetarian: "yes" },
+  { type: "sauce", name: "ketchup", vegetarian: "yes" },
+  { type: "sauce", name: "barbeque", vegetarian: "yes" },
+  { type: "sauce", name: "hot sauce", vegetarian: "yes" },
+  { type: "sauce", name: "a1", vegetarian: "yes" },
+];
