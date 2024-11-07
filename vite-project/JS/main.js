3804bcb6ec40342a76a13cd4da898149b530f014
isaach55 <isaach55@nycstudents.net>
2024-11-07 12:29:04 -0500

    start formatting

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -12,51 +12,60 @@ const DOMSelectors = {
   container: document.getElementById("container"),
 };
 
-DOMSelectors.pattyButton.addEventListener("click", function() {
-  const pattyProducts = products.filter(product => product.type === "patty");
+DOMSelectors.pattyButton.addEventListener("click", function () {
+  const pattyProducts = products.filter((product) => product.type === "patty");
   createCard(pattyProducts);
 });
 
-DOMSelectors.toppingButton.addEventListener("click", function() {
-  const toppingProducts = products.filter(product => product.type === "topping");
+DOMSelectors.toppingButton.addEventListener("click", function () {
+  const toppingProducts = products.filter(
+    (product) => product.type === "topping"
+  );
   createCard(toppingProducts);
 });
 
-DOMSelectors.cheeseButton.addEventListener("click", function() {
-  const cheeseProducts = products.filter(product => product.type === "cheese");
+DOMSelectors.cheeseButton.addEventListener("click", function () {
+  const cheeseProducts = products.filter(
+    (product) => product.type === "cheese"
+  );
   createCard(cheeseProducts);
 });
 
-DOMSelectors.sauceButton.addEventListener("click", function() {
-  const sauceProducts = products.filter(product => product.type === "sauce");
+DOMSelectors.sauceButton.addEventListener("click", function () {
+  const sauceProducts = products.filter((product) => product.type === "sauce");
   createCard(sauceProducts);
 });
 
-DOMSelectors.vegetarianButton.addEventListener("click", function() {
-  const vegetarianProducts = products.filter(product => product.vegetarian);
+DOMSelectors.vegetarianButton.addEventListener("click", function () {
+  const vegetarianProducts = products.filter((product) => product.vegetarian);
   createCard(vegetarianProducts);
 });
 
-DOMSelectors.saleButton.addEventListener("click", function() {
+DOMSelectors.saleButton.addEventListener("click", function () {
   const saleProducts = products.map((product) => {
-    return {...product, price: product.price * 0.75}
+    return { ...product, price: product.price * 0.75 };
   });
   createCard(saleProducts);
 });
 
-DOMSelectors.resetButton.addEventListener("click", function() {
-  console.log("click")
+DOMSelectors.resetButton.addEventListener("click", function () {
+  console.log("click");
   createCard(products);
 });
 
 function createCard(products) {
-  DOMSelectors.container.innerHTML = '';
-  products.forEach(product => {
-  DOMSelectors.container.insertAdjacentHTML('beforeend',
-    `<div class="card">
+  DOMSelectors.container.innerHTML = "";
+  products.forEach((product) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+      <img src="${products.image}" alt="${}" width=10vw >
       <p>${product.name}</p>
       <p>$${product.price}</p>
     </div>`
-  );
-});
-};
+    );
+  });
+}
+//      <img src="${products.image}" alt="${"
+
+createCard(products);
