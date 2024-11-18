462c0b8a5ae51f2161ffea468b031ffa6d107fbf
isaach55 <isaach55@nycstudents.net>
2024-11-18 01:59:35 -0500

    finish

--- vite-project/JS/index.js
+++ vite-project/JS/index.js
@@ -2,6 +2,7 @@ import "../css/style.css";
 import { products } from "./products.js";
 
 const DOMSelectors = {
+  body: document.body,
   pattyButton: document.getElementById("patty"),
   toppingButton: document.getElementById("topping"),
   cheeseButton: document.getElementById("cheese"),
@@ -10,6 +11,7 @@ const DOMSelectors = {
   saleButton: document.getElementById("sale"),
   resetButton: document.getElementById("reset"),
   container: document.getElementById("container"),
+  themeButton: document.getElementById("theme-button")
 };
 
 DOMSelectors.pattyButton.addEventListener("click", function () {
@@ -43,25 +45,35 @@ DOMSelectors.vegetarianButton.addEventListener("click", function () {
 
 DOMSelectors.saleButton.addEventListener("click", function () {
   const saleProducts = products.map((product) => {
-    return { ...product, price: (product.price * .75).toFixed(2)};
+    return { ...product, price: parseFloat((product.price * .75).toFixed(2))};
   });
   createCard(saleProducts);
 });
 
 DOMSelectors.resetButton.addEventListener("click", function () {
-  console.log("click");
   createCard(products);
+});
+
+DOMSelectors.themeButton.addEventListener("click", function () {
+  if (DOMSelectors.body.classList.contains("blue")) {
+    DOMSelectors.body.classList.add("black");
+    DOMSelectors.body.classList.remove("blue");
+  } else {
+    DOMSelectors.body.classList.add("blue");
+    DOMSelectors.body.classList.remove("black");
+  }
 });
 
 function createCard(products) {
   DOMSelectors.container.innerHTML = "";
   products.forEach((product) => {
+    const roundedPrice = (product.price).toFixed(2);
     DOMSelectors.container.insertAdjacentHTML(
       "beforeend",
       `<div class="card">
       <img src="${product.image}" alt="${product.name}" class="product-image">
       <p class="product-name">${product.name}</p>
-      <p class="product-price">$${product.price}</p>
+      <p class="product-price">${"$"+roundedPrice}</p>
     </div>`
     );
   });
